0d833be0cf7f2d38f80acd33238a60368230d6e1
Mike Bostock <mbostock@gmail.com>
2015-07-07 07:56:38 -0700

    Consolidate basis code.

--- src/interpolate/basis.js
+++ src/interpolate/basis.js
@@ -1,3 +1,14 @@
+function basisPoint(basis, x, y) {
+  basis._context.bezierCurveTo(
+    (2 * basis._x0 + basis._x1) / 3,
+    (2 * basis._y0 + basis._y1) / 3,
+    (basis._x0 + 2 * basis._x1) / 3,
+    (basis._y0 + 2 * basis._y1) / 3,
+    (basis._x0 + 4 * basis._x1 + x) / 6,
+    (basis._y0 + 4 * basis._y1 + y) / 6
+  );
+}
+
 export function Basis(context) {
   this._context = context;
 };
@@ -8,13 +19,15 @@ Basis.prototype.lineStart = function() {
 };
 
 Basis.prototype.lineEnd = function() {
-  if (this._x0 != null) this._context.bezierCurveTo((2 * this._x0 + this._x1) / 3, (2 * this._y0 + this._y1) / 3, (this._x0 + 2 * this._x1) / 3, (this._y0 + 2 * this._y1) / 3, (this._x0 + 5 * this._x1) / 6, (this._y0 + 5 * this._y1) / 6);
-  if (this._x1 != null) this._context.lineTo(this._x1, this._y1);
+  if (this._x0 != null) {
+    this.point(this._x1, this._y1);
+    this._context.lineTo(this._x1, this._y1);
+  }
 };
 
 Basis.prototype.point = function(x, y) {
   x = +x, y = +y;
-  if (this._x0 != null) this._context.bezierCurveTo((2 * this._x0 + this._x1) / 3, (2 * this._y0 + this._y1) / 3, (this._x0 + 2 * this._x1) / 3, (this._y0 + 2 * this._y1) / 3, (this._x0 + 4 * this._x1 + x) / 6, (this._y0 + 4 * this._y1 + y) / 6);
+  if (this._x0 != null) basisPoint(this, x, y);
   else if (this._x1 != null) this._context.lineTo((5 * this._x1 + x) / 6, (5 * this._y1 + y) / 6);
   else this._context.moveTo(x, y);
   this._x0 = this._x1, this._x1 = x;
@@ -35,7 +48,7 @@ BasisOpen.prototype.lineEnd = function() {};
 
 BasisOpen.prototype.point = function(x, y) {
   x = +x, y = +y;
-  if (this._moved) this._context.bezierCurveTo((2 * this._x0 + this._x1) / 3, (2 * this._y0 + this._y1) / 3, (this._x0 + 2 * this._x1) / 3, (this._y0 + 2 * this._y1) / 3, (this._x0 + 4 * this._x1 + x) / 6, (this._y0 + 4 * this._y1 + y) / 6);
+  if (this._moved) basisPoint(this, x, y);
   else if (this._x0 != null) this._moved = true, this._context.moveTo((this._x0 + 4 * this._x1 + x) / 6, (this._y0 + 4 * this._y1 + y) / 6);
   this._x0 = this._x1, this._x1 = x;
   this._y0 = this._y1, this._y1 = y;
@@ -58,7 +71,7 @@ BasisClosed.prototype.lineEnd = function() {
 
 BasisClosed.prototype.point = function(x, y) {
   x = +x, y = +y;
-  if (this._x4 != null) this._context.bezierCurveTo((2 * this._x0 + this._x1) / 3, (2 * this._y0 + this._y1) / 3, (this._x0 + 2 * this._x1) / 3, (this._y0 + 2 * this._y1) / 3, (this._x0 + 4 * this._x1 + x) / 6, (this._y0 + 4 * this._y1 + y) / 6);
+  if (this._x4 != null) basisPoint(this, x, y);
   else if (this._x3 != null) this._x4 = x, this._y4 = y, this._context.moveTo((this._x0 + 4 * this._x1 + x) / 6, (this._y0 + 4 * this._y1 + y) / 6);
   else if (this._x2 != null) this._x3 = x, this._y3 = y;
   else this._x2 = x, this._y2 = y;
