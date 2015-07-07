52a483bdc3d280c0128ca5d606a35ce70216b058
Mike Bostock <mbostock@gmail.com>
2015-07-07 08:13:25 -0700

    Fix basisClosed.lineEnd.

--- src/interpolate/basis.js
+++ src/interpolate/basis.js
@@ -64,9 +64,18 @@ BasisClosed.prototype.lineStart = function() {
 };
 
 BasisClosed.prototype.lineEnd = function() {
-  if (this._x2 != null) this.point(this._x2, this._y2);
-  if (this._x3 != null) this.point(this._x3, this._y3);
-  if (this._x4 != null) this.point(this._x4, this._y4);
+  if (this._x4 != null) {
+    this.point(this._x2, this._y2);
+    this.point(this._x3, this._y3);
+    this.point(this._x4, this._y4);
+  } else if (this._x3 != null) {
+    this._context.moveTo((this._x2 + 2 * this._x3) / 3, (this._y2 + 2 * this._y3) / 3);
+    this._context.lineTo((this._x3 + 2 * this._x2) / 3, (this._y3 + 2 * this._y2) / 3);
+    this._context.closePath();
+  } else if (this._x2 != null) {
+    this._context.moveTo(this._x2, this._y2);
+    this._context.closePath();
+  }
 };
 
 BasisClosed.prototype.point = function(x, y) {
