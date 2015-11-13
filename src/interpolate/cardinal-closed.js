a56b4efa1c8f3e6461554390bf07981623cc7e78
Mike Bostock <mbostock@gmail.com>
2015-11-12 21:35:55 -0800

    Remove unused fields.

--- src/interpolate/cardinal-closed.js
+++ src/interpolate/cardinal-closed.js
@@ -11,26 +11,26 @@ function CardinalClosed(context, tension) {
 
 CardinalClosed.prototype = {
   lineStart: function() {
-    this._x0 = this._x1 = this._x2 = this._x3 = this._x4 = this._x5 = this._x6 =
-    this._y0 = this._y1 = this._y2 = this._y3 = this._y4 = this._y5 = this._y6 = NaN;
+    this._x0 = this._x1 = this._x2 = this._x3 = this._x4 = this._x5 =
+    this._y0 = this._y1 = this._y2 = this._y3 = this._y4 = this._y5 = NaN;
     this._state = 0;
   },
   lineEnd: function() {
     switch (this._state) {
       case 1: {
-        this._context.moveTo(this._x4, this._y4);
+        this._context.moveTo(this._x3, this._y3);
         this._context.closePath();
         break;
       }
       case 2: {
-        this._context.lineTo(this._x4, this._y4);
+        this._context.lineTo(this._x3, this._y3);
         this._context.closePath();
         break;
       }
       case 3: {
+        this.point(this._x3, this._y3);
         this.point(this._x4, this._y4);
         this.point(this._x5, this._y5);
-        this.point(this._x6, this._y6);
         break;
       }
     }
@@ -38,9 +38,9 @@ CardinalClosed.prototype = {
   point: function(x, y) {
     x = +x, y = +y;
     switch (this._state) {
-      case 0: this._state = 1; this._x4 = x, this._y4 = y; break;
-      case 1: this._state = 2; this._x5 = x, this._y5 = y; this._context.moveTo(x, y); break;
-      case 2: this._state = 3; this._x6 = x, this._y6 = y; break;
+      case 0: this._state = 1; this._x3 = x, this._y3 = y; break;
+      case 1: this._state = 2; this._x4 = x, this._y4 = y; this._context.moveTo(x, y); break;
+      case 2: this._state = 3; this._x5 = x, this._y5 = y; break;
       default: {
         this._context.bezierCurveTo(
           this._x1 + this._k * (this._x2 - this._x0),
