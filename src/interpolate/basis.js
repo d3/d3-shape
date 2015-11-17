1ec9a62678707b2d4e9e6afc732b608f42599c7c
Mike Bostock <mbostock@gmail.com>
2015-11-16 17:38:55 -0800

    Combine line segments when n = 2.

--- src/interpolate/basis.js
+++ src/interpolate/basis.js
@@ -34,8 +34,8 @@ Basis.prototype = {
     x = +x, y = +y;
     switch (this._state) {
       case 0: this._state = 1; this._context.moveTo(x, y); break;
-      case 1: this._state = 2; this._context.lineTo((5 * this._x1 + x) / 6, (5 * this._y1 + y) / 6); break;
-      case 2: this._state = 3; // proceed
+      case 1: this._state = 2; break;
+      case 2: this._state = 3; this._context.lineTo((5 * this._x0 + this._x1) / 6, (5 * this._y0 + this._y1) / 6); // proceed
       default: point(this, x, y); break;
     }
     this._x0 = this._x1, this._x1 = x;
