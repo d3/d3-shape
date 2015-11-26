b03626702079a7f6c392c5c89ef10b5a7c7d0796
Mike Bostock <mbostock@gmail.com>
2015-11-26 13:24:57 -0800

    Minor tweaks.

--- src/curve/monotone.js
+++ src/curve/monotone.js
@@ -12,32 +12,28 @@ function slope(that, x, y) {
       s0 = h0 === 0 ? 0 : (that._y1 - that._y0) / h0,
       s1 = h1 === 0 ? 0 : (y - that._y1) / h1,
       p = h0 + h1 === 0 ? 0 : (s0 * h1 + s1 * h0) / (h0 + h1);
-
   return (sign(s0) + sign(s1)) * Math.min(Math.abs(s0), Math.abs(s1), 0.5 * Math.abs(p));
 }
 
+// According to https://en.wikipedia.org/wiki/Cubic_Hermite_spline#Representations
+// "you can express cubic Hermite interpolation in terms of cubic Bézier curves
+// with respect to the four values p0, p0 + m0 / 3, p1 - m1 / 3, p1".
 function point(that, t1) {
-  var dx,
-      x0 = that._x0,
+  var x0 = that._x0,
       y0 = that._y0,
       x1 = that._x1,
       y1 = that._y1,
-      t0 = that._t0;
-
-  // According to https://en.wikipedia.org/wiki/Cubic_Hermite_spline#Representations
-  // "you can express cubic Hermite interpolation in terms of cubic
-  // Bézier curves with respect to the four values p0, p0 + m0 / 3, p1 -
-  // m1 / 3, p1".
-  dx = (x1 - x0) / 3.0;
+      t0 = that._t0,
+      dx = (x1 - x0) / 3.0;
   that._context.bezierCurveTo(x0 + dx, y0 + dx * t0, x1 - dx, y1 - dx * t1, x1, y1);
 }
 
 function monotone(context) {
-    return new Monotone(context);
+  return new Monotone(context);
 }
 
 function Monotone(context) {
-    this._context = context;
+  this._context = context;
 }
 
 Monotone.prototype = {
@@ -70,6 +66,7 @@ Monotone.prototype = {
       case 2: this._point = 3; // proceed
       default: t1 = slope(this, x, y); point(this, t1); break;
     }
+
     this._x0 = this._x1, this._x1 = x;
     this._y0 = this._y1, this._y1 = y;
     this._t0 = t1;
