c1a9743e92af8d30408b6b4217e39d231aa35306
Mike Bostock <mbostock@gmail.com>
2015-11-27 21:06:56 -0800

    Fixes for duplicate x-values.
    If the one-sided slope is undefined, or if one of the two sides of a two-sided
    slope is undefined, use the other slope. If there are coincident points, assume
    the slope is zero.

--- src/curve/monotone.js
+++ src/curve/monotone.js
@@ -9,16 +9,16 @@ function sign(x) {
 function slope3(that, x2, y2) {
   var h0 = that._x1 - that._x0,
       h1 = x2 - that._x1,
-      s0 = h0 === 0 ? 0 : (that._y1 - that._y0) / h0,
-      s1 = h1 === 0 ? 0 : (y2 - that._y1) / h1,
-      p = h0 + h1 === 0 ? 0 : (s0 * h1 + s1 * h0) / (h0 + h1);
-  return (sign(s0) + sign(s1)) * Math.min(Math.abs(s0), Math.abs(s1), 0.5 * Math.abs(p));
+      s0 = (that._y1 - that._y0) / h0,
+      s1 = (y2 - that._y1) / h1,
+      p = (s0 * h1 + s1 * h0) / (h0 + h1);
+  return (sign(s0) + sign(s1)) * Math.min(Math.abs(s0), Math.abs(s1), 0.5 * Math.abs(p)) || 0;
 }
 
 // Calculate a one-sided slope.
-function slope2(that) {
+function slope2(that, t) {
   var h = that._x1 - that._x0;
-  return h === 0 ? 0 : (that._y1 - that._y0) / h;
+  return h ? (that._y1 - that._y0) / h : t;
 }
 
 // According to https://en.wikipedia.org/wiki/Cubic_Hermite_spline#Representations
@@ -29,7 +29,7 @@ function point(that, t0, t1) {
       y0 = that._y0,
       x1 = that._x1,
       y1 = that._y1,
-      dx = (x1 - x0) / 3.0;
+      dx = (x1 - x0) / 3;
   that._context.bezierCurveTo(x0 + dx, y0 + dx * t0, x1 - dx, y1 - dx * t1, x1, y1);
 }
 
@@ -56,7 +56,7 @@ Monotone.prototype = {
   lineEnd: function() {
     switch (this._point) {
       case 2: this._context.lineTo(this._x1, this._y1); break;
-      case 3: point(this, this._t0, 3 * slope2(this) / 2 - this._t0 / 2); break;
+      case 3: point(this, this._t0, 3 * slope2(this, this._t0) / 2 - this._t0 / 2); break;
     }
     if (this._line || (this._line !== 0 && this._point === 1)) this._context.closePath();
     this._line = 1 - this._line;
@@ -68,7 +68,7 @@ Monotone.prototype = {
     switch (this._point) {
       case 0: this._point = 1; this._line ? this._context.lineTo(x, y) : this._context.moveTo(x, y); break;
       case 1: this._point = 2; break;
-      case 2: this._point = 3; t1 = slope3(this, x, y); point(this, 3 * slope2(this) / 2 - t1 / 2, t1); break;
+      case 2: this._point = 3; t1 = slope3(this, x, y); point(this, 3 * slope2(this, t1) / 2 - t1 / 2, t1); break;
       default: t1 = slope3(this, x, y); point(this, this._t0, t1); break;
     }
 
