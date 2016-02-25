fa5265ec9ff89b67c404c027f1107d758a416150
Mike Bostock <mbostock@gmail.com>
2016-02-24 16:20:27 -0800

    Update d3-polygon.

--- test/polygonContext.js
+++ test/polygonContext.js
@@ -3,7 +3,7 @@ var polygon = require("d3-polygon");
 module.exports = function() {
   return {
     points: null,
-    area: function() { return Math.abs(polygon.area(this.points)); },
+    area: function() { return Math.abs(polygon.polygonArea(this.points)); },
     moveTo: function(x, y) { this.points = [[x, y]]; },
     lineTo: function(x, y) { this.points.push([x, y]); },
     rect: function(x, y, w, h) { this.points = [[x, y], [x + w, y], [x + w, y + h], [x, y + h]]; },
