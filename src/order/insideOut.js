141d93cf64ce7fa7af29912abb40cfb32d46d44c
ZijingP <zjpeng789@outlook.com>
2018-07-28 11:34:56 -0400

    fix(insideOut.js): fix the bug that stack orders do not show by onset time when it is a ThemeRiver

--- src/order/insideOut.js
+++ src/order/insideOut.js
@@ -5,8 +5,9 @@ export default function(series) {
   var n = series.length,
       i,
       j,
+      onset = series.map(getFirst),
+      order = none(series).sort(function(a, b) { return onset[a] - onset[b]; }),
       sums = series.map(sum),
-      order = none(series).sort(function(a, b) { return sums[b] - sums[a]; }),
       top = 0,
       bottom = 0,
       tops = [],
@@ -22,6 +23,15 @@ export default function(series) {
       bottoms.push(j);
     }
   }
-
   return bottoms.reverse().concat(tops);
+}
+
+export function getFirst(series) {
+  var s = 0, i = -1, n = series.length, v;
+  while (++i < n) {
+    if (series[i][1] - series[i][0]> 0) {
+      return i;
+    }
+  }
+  return n-1;
 }
