0aff62f7c8397884998c52a224db0c3495178466
Mike Bostock <mbostock@gmail.com>
2019-01-23 13:44:46 -0800

    Fix tests.

--- test/order/appearance-test.js
+++ test/order/appearance-test.js
@@ -3,18 +3,18 @@ var tape = require("tape"),
 
 tape("stackOrderAppearance(series) returns an order by appearance", function(test) {
   test.deepEqual(shape.stackOrderAppearance([
-    [[0, 0], [0, 0], [0, 0]],
-    [[0, 0], [0, 2], [0, 3]],
-    [[0, 0], [0, 0], [0, 4]]
+    [[0, 0], [0, 0], [0, 1]],
+    [[0, 3], [0, 2], [0, 0]],
+    [[0, 0], [0, 4], [0, 0]]
   ]), [1, 2, 0]);
   test.end();
 });
 
 tape("stackOrderAppearance(series) treats NaN values as zero", function(test) {
   test.deepEqual(shape.stackOrderAppearance([
-    [[0, 0], [0, NaN], [0, NaN]],
-    [[0, 0], [0, 2], [0, 3]],
-    [[0, 0], [0, NaN], [0, 4]]
+    [[0, NaN], [0, NaN], [0, 1]],
+    [[0, 3], [0, 2], [0, NaN]],
+    [[0, NaN], [0, 4], [0, NaN]]
   ]), [1, 2, 0]);
   test.end();
 });
