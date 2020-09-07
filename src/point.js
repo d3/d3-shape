6f9c047ec8b80f6bc055b314c2c54e615df50a5d
wuyanhong <772781043@qq.com>
2020-09-07 09:09:24 +0800

    cleaner merge

--- src/point.js
+++ src/point.js
@@ -6,7 +6,6 @@ export function y(p) {
   return p[1];
 }
 
-//the return of d3.trail().size()
-export function z(p){
+export function z(p) {
   return p[2];
 }
--- src/trail.js
+++ src/trail.js
@@ -3,44 +3,44 @@ import constant from "./constant.js";
 import {x as pointX, y as pointY, z as pointSize} from "./point.js";
 import {abs, atan2, cos, pi, tau, sin, sqrt, max, min} from "./math.js";
 
-export default function(x, y, size){
+export default function(x, y, size) {
   var defined = constant(true),
       context = null,
-      ready, 
+      ready,
       ready2,
       scaleRatio_min = 1,
       ct1,
       px1, py1, pr1,
-      arp = []; 
+      arp = [];
 
   x = typeof x === "function" ? x : (x === undefined) ? pointX : constant(x);
   y = typeof y === "function" ? y : (y === undefined) ? pointY : constant(y);
   size = typeof size === "function" ? size : (size === undefined) ? pointSize : constant(size);
 
-  function cross(x1, y1, x2, y2){
+  function cross(x1, y1, x2, y2) {
     return x1 * y2 - x2 * y1;
   }
-  
-  function isIntersect(x1, y1, x2, y2, x3, y3, x4, y4){
-    //rapid repulsion
-    if(max(x3, x4) < min(x1, x2) || max(y3, y4) < min(y1, y2) || max(x1, x2) < min(x3, x4) || max(y1, y2) < min(y3, y4)){
+
+  function isIntersect(x1, y1, x2, y2, x3, y3, x4, y4) {
+    // rapid repulsion
+    if (max(x3, x4) < min(x1, x2) || max(y3, y4) < min(y1, y2) || max(x1, x2) < min(x3, x4) || max(y1, y2) < min(y3, y4)) {
       return false;
     }
-    
-    //straddle experiment
-    if(cross(x1 - x4, y1 - y4, x3 - x4, y3 - y4)*cross(x2 - x4, y2 - y4, x3 - x4, y3 - y4) > 0 ||
-       cross(x3 - x2, y3 - y2, x1 - x2, y1 - y2)*cross(x4 - x2, y4 - y2, x1 - x2, y1 - y2) > 0){
+
+    // straddle experiment
+    if (cross(x1 - x4, y1 - y4, x3 - x4, y3 - y4) * cross(x2 - x4, y2 - y4, x3 - x4, y3 - y4) > 0 ||
+       cross(x3 - x2, y3 - y2, x1 - x2, y1 - y2) * cross(x4 - x2, y4 - y2, x1 - x2, y1 - y2) > 0) {
       return false;
     }
     return true;
   }
 
-  function intersectPoint(x1, y1, x2, y2, x3, y3, x4, y4){
+  function intersectPoint(x1, y1, x2, y2, x3, y3, x4, y4) {
     var d1 = abs(cross(x4 - x3, y4 - y3, x1 - x3, y1 - y3)),
         d2 = abs(cross(x4 - x3, y4 - y3, x2 - x3, y2 - y3)),
         t;
 
-    if(!(d1 || d2)){
+    if (!(d1 || d2)) {
       return {
         x: x2,
         y: y2
@@ -52,25 +52,25 @@ export default function(x, y, size){
         x: x1 + (x2 - x1) * t,
         y: y1 + (y2 - y1) * t
       };
-    }  
+    }
   }
 
-  function commonTangent(x1, y1, w1, x2, y2, w2){
+  function commonTangent(x1, y1, w1, x2, y2, w2) {
     var r1 = w1 / 2,
-        r2 = w2 / 2, 
-        alpha, //the smallest angle in right-angled trapezoid
+        r2 = w2 / 2,
+        alpha, // the smallest angle in right-angled trapezoid
         alpha_x,
         alpha_y,
-        beta, //the slope of line segment
+        beta, // the slope of line segment
         cos_x,
         sin_y;
-        
+
     alpha_x = abs(r2 - r1);
     alpha_y = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2) - alpha_x * alpha_x);
     alpha = atan2(alpha_y, alpha_x);
     beta = atan2(y2 - y1, x2 - x1);
 
-    if(r1 < r2){
+    if (r1 < r2) {
       cos_x = -cos(alpha + beta);
       sin_y = -sin(alpha + beta);
 
@@ -96,13 +96,12 @@ export default function(x, y, size){
     }
   }
 
-  function segment(x1, y1, w1, x2, y2, w2){
+  function segment(x1, y1, w1, x2, y2, w2) {
     var ct2 = commonTangent(x1, y1, w1, x2, y2, w2);
 
-    if(ready){
-      if(isIntersect(ct1.sg1_x, ct1.sg1_y, ct1.sg2_x, ct1.sg2_y, ct2.sg1_x, ct2.sg1_y, ct2.sg2_x, ct2.sg2_y)){
-        var p = intersectPoint(ct1.sg1_x, ct1.sg1_y, ct1.sg2_x, ct1.sg2_y, ct2.sg1_x, ct2.sg1_y, ct2.sg2_x, ct2.sg2_y);
-            
+    if (ready) {
+      if(isIntersect(ct1.sg1_x, ct1.sg1_y, ct1.sg2_x, ct1.sg2_y, ct2.sg1_x, ct2.sg1_y, ct2.sg2_x, ct2.sg2_y)) {
+        var p = intersectPoint(ct1.sg1_x, ct1.sg1_y, ct1.sg2_x, ct1.sg2_y, ct2.sg1_x, ct2.sg1_y, ct2.sg2_x, ct2.sg2_y);    
         context.lineTo(p.x, p.y);
       }
       else{
@@ -117,22 +116,22 @@ export default function(x, y, size){
     ct1 = ct2;
   }
 
-  function scaleRatio(px2, py2, pw2, i){
+  function scaleRatio(px2, py2, pw2, i) {
     var pr2 = pw2 / 2;
 
-    if(ready2){
+    if (ready2) {
       var lxy = sqrt((px1 - px2) * (px1 - px2) + (py1 - py2) * (py1 - py2));
-      if(lxy){
-        if(pr1 + pr2 > lxy){
+      if (lxy > 1) {
+        if (pr1 + pr2 > lxy) {
           var sr = lxy / (pr1 + pr2);
-          if(sr < scaleRatio_min){
+          if (sr < scaleRatio_min) {
             scaleRatio_min = sr;
-          } 
+          }
         }
         arp[i] = false;
       }
       else{
-        arp[i] = true; //when p1.x == p2.x and p1.y == p2.y
+        arp[i] = true; // when p1.x == p2.x and p1.y == p2.y
       }
     }
     else{
@@ -144,31 +143,31 @@ export default function(x, y, size){
     pr1 = pr2;
   }
 
-  function trail(data){ 
+  function trail(data) { 
     var i,
-        n = data.length, 
+        n = data.length,
         d,
         def,
         defined0 = false,
         defined1 = false,
         buffer,
-        arx = [], //arrays of x(), y(), size(), defined()
+        arx = [], // arrays of x(), y(), size(), defined()
         ary = [],
         ars = [],
         ard = [];
 
-    //make global optimization for radius when r1 + r2 > lxy
-    for(i = 0; i < n; i++){
+    // make global optimization for radius when r1 + r2 > lxy
+    for (i = 0; i < n; i++) {
       def = (ard[i] = defined(d = data[i], i, data)) && (ars[i] = +size(d, i, data));
-      if(!(i < n && def) === defined1){
+      if (!(i < n && def) === defined1) {
         if (defined1 = !defined1) ready2 = 0;
       }
-      if(defined1) {
+      if (defined1) {
         scaleRatio(arx[i] = +x(d, i, data), ary[i] = +y(d, i, data), ars[i], i);
       }
     }
 
-    if(context == null) context = buffer = path();
+    if (context == null) context = buffer = path();
 
     var start,
         j,
@@ -176,20 +175,20 @@ export default function(x, y, size){
         def1,
         tag = false;
 
-    for(i = 0; i < n; i++){
+    for (i = 0; i < n; i++) {
       def = ard[i] && ars[i];
-      if(!(i < n && def) === defined0){
-        if(defined0 = !defined0){
+      if (!(i < n && def) === defined0) {
+        if (defined0 = !defined0) {
           ready = 0;
           start = i;
         }
       }
-      if(defined0){
+      if (defined0) {
         j = i + 1;
-        if(j < n){
+        if (j < n) {
           def1 = ard[j] && ars[j];
-          if(def1){
-            if(arp[j]){
+          if (def1) {
+            if (arp[j]) {
               tag = true;
             }
             else{
@@ -203,16 +202,16 @@ export default function(x, y, size){
         else{
           tag = true;
         }
-        if(tag){
+        if (tag) {
           j = i - 1;
-          if(j < start){
-            //draw a circle
+          if (j < start) {
+            // draw a circle
             context.moveTo(arx[i] + ars[i] * scaleRatio_min / 2, ary[i]);
             context.arc(arx[i], ary[i], ars[i] * scaleRatio_min / 2, 0, tau, 0);
             context.closePath();
           }
           else{
-            while(j >= start){
+            while(j >= start) {
               d1 = data[j];
               segment(arx[j + 1], ary[j + 1], ars[j + 1] * scaleRatio_min, arx[j], ary[j], ars[j] * scaleRatio_min);
               d = d1;
@@ -231,29 +230,29 @@ export default function(x, y, size){
       }
     }
 
-    if(buffer){
+    if (buffer) {
       context = null;
       return buffer + '' || null;
     }
   }
 
-  trail.x = function(_){
+  trail.x = function(_) {
     return arguments.length ? (x = typeof _ === "function" ? _ : constant(+_), trail) : x;
   };
 
-  trail.y = function(_){
+  trail.y = function(_) {
     return arguments.length ? (y = typeof _ === "function" ? _ : constant(+_), trail) : y;
   };
 
-  trail.size = function(_){
+  trail.size = function(_) {
     return arguments.length ? (size = typeof _ === "function" ? _ : constant(+_), trail) : size;
   };
 
-  trail.defined = function(_){
+  trail.defined = function(_) {
     return arguments.length ? (defined = typeof _ === "function" ? _ : constant(!!_), trail) : defined;
   };
 
-  trail.context = function(_){
+  trail.context = function(_) {
     return arguments.length ? ((context = _ == null ? null : _), trail) : context;
   };
 
