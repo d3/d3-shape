c8aef6c36d8b5685f6ed947d5a85af032fce0a89
Mike Bostock <mbostock@gmail.com>
2022-12-20 14:55:50 -0800

    update dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import {readFileSync} from "fs";
 import {terser} from "rollup-plugin-terser";
-import * as meta from "./package.json";
+import meta from "./package.json" assert {type: "json"};
 
 // Extract copyrights from the LICENSE.
 const copyright = readFileSync("./LICENSE", "utf-8")
